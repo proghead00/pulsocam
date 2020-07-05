edbf01b834f9c94dff0b94646b46773a1bef08f8
proghead00 <susnata00@gmail.com>
2020-07-05 21:04:11 +0530

    OpenCV project

---
+++ get_pulse.py
@@ -0,0 +1,180 @@
+from lib.device import Camera
+from lib.processors_noopenmdao import findFaceGetPulse
+from lib.interface import plotXY, imshow, waitKey, destroyWindow
+from cv2 import moveWindow
+import argparse
+import numpy as np
+import datetime
+
+import socket
+import sys
+
+class getPulseApp(object):
+
+    
+
+    def __init__(self, args):
+        #tried to use droidcam from playstore,this won't work like that
+        serial = args.serial
+        baud = args.baud
+        self.send_serial = False
+        self.send_udp = False
+        if serial:
+            self.send_serial = True
+            if not baud:
+                baud = 9600
+            else:
+                baud = int(baud)
+            self.serial = Serial(port=serial, baudrate=baud)
+
+        udp = args.udp
+        if udp:
+            self.send_udp = True
+            if ":" not in udp:
+                ip = udp
+                port = 5005
+            else:
+                ip, port = udp.split(":")
+                port = int(port)
+            self.udp = (ip, port)
+            self.sock = socket.socket(socket.AF_INET, # Internet
+                 socket.SOCK_DGRAM) # UDP
+
+        self.cameras = []
+        self.selected_cam = 0
+        for i in range(3):
+            camera = Camera(camera=i)  # first camera by default
+            if camera.valid or not len(self.cameras):
+                self.cameras.append(camera)
+            else:
+                break
+        self.w, self.h = 0, 0
+        self.pressed = 0
+      
+        self.processor = findFaceGetPulse(bpm_limits=[50, 160],
+                                          data_spike_limit=2500.,
+                                          face_detector_smoothness=10.)
+
+  
+        self.bpm_plot = False
+        self.plot_title = "Data display - raw signal (top) and PSD (bottom)"
+
+       
+        self.key_controls = {"s": self.toggle_search,
+                             "d": self.toggle_display_plot,
+                             "c": self.toggle_cam,
+                             "f": self.write_csv}
+
+    def toggle_cam(self):
+        if len(self.cameras) > 1:
+            self.processor.find_faces = True
+            self.bpm_plot = False
+            destroyWindow(self.plot_title)
+            self.selected_cam += 1
+            self.selected_cam = self.selected_cam % len(self.cameras)
+
+    def write_csv(self):
+        """
+        Writes current data to a csv file
+        """
+        fn = "Webcam-pulse" + str(datetime.datetime.now())
+        fn = fn.replace(":", "_").replace(".", "_")
+        data = np.vstack((self.processor.times, self.processor.samples)).T
+        np.savetxt(fn + ".csv", data, delimiter=',')
+        print("Writing csv")
+
+    def toggle_search(self):
+       
+        #state = self.processor.find_faces.toggle()
+        state = self.processor.find_faces_toggle()
+        print("face detection lock =", not state)
+
+    def toggle_display_plot(self):
+       
+        if self.bpm_plot:
+            print("bpm plot disabled")
+            self.bpm_plot = False
+            destroyWindow(self.plot_title)
+        else:
+            print("bpm plot enabled")
+            if self.processor.find_faces:
+                self.toggle_search()
+            self.bpm_plot = True
+            self.make_bpm_plot()
+            moveWindow(self.plot_title, self.w, 0)
+
+    def make_bpm_plot(self):
+        """
+        Creates and/or updates the data display
+        """
+        plotXY([[self.processor.times,
+                 self.processor.samples],
+                [self.processor.freqs,
+                 self.processor.fft]],
+               labels=[False, True],
+               showmax=[False, "bpm"],
+               label_ndigits=[0, 0],
+               showmax_digits=[0, 1],
+               skip=[3, 3],
+               name=self.plot_title,
+               bg=self.processor.slices[0])
+
+    def key_handler(self):
+        
+
+        self.pressed = waitKey(10) & 255  # wait for keypress for 10 ms
+        if self.pressed == 27:  # exit program on 'esc'
+            print("Exiting")
+            for cam in self.cameras:
+                cam.cam.release()
+            if self.send_serial:
+                self.serial.close()
+            sys.exit()
+
+        for key in self.key_controls.keys():
+            if chr(self.pressed) == key:
+                self.key_controls[key]()
+
+    def main_loop(self):
+        
+        frame = self.cameras[self.selected_cam].get_frame()
+        self.h, self.w, _c = frame.shape
+
+        
+
+        # set current image frame to the processor's input
+        self.processor.frame_in = frame
+        # process the image frame to perform all needed analysis5
+        self.processor.run(self.selected_cam)
+        # collect the output frame for display
+        output_frame = self.processor.frame_out
+
+        # show the processed/annotated output frame
+        imshow("Processed", output_frame)
+
+        # create and/or update the raw data display if needed
+        if self.bpm_plot:
+            self.make_bpm_plot()
+
+        if self.send_serial:
+            self.serial.write(str(self.processor.bpm) + "\r\n")
+
+        if self.send_udp:
+            self.sock.sendto(str(self.processor.bpm), self.udp)
+
+        # handle any key presses
+        self.key_handler()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Pulsocam')
+    parser.add_argument('--serial', default=None,
+                        help='serial port destination for bpm data')
+    parser.add_argument('--baud', default=None,
+                        help='Baud rate for serial transmission')
+    parser.add_argument('--udp', default=None,
+                        help='udp address:port destination for bpm data')
+
+    args = parser.parse_args()
+    App = getPulseApp(args)
+    while True:
+        App.main_loop()
